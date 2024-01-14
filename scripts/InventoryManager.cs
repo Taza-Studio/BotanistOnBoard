d44303cb8447cc1defb475d9eb2c22f17a3beff2
giggymantis <hypegroupyt@gmail.com>
2024-01-14 14:17:29 -0500

    Basic player and inventory code setup

---
+++ scripts/InventoryManager.cs
@@ -0,0 +1,7 @@
+using Godot;
+using System;
+
+public partial class InventoryManager
+{
+	public ItemSlot[] inventory = new ItemSlot[10];
+}
---
+++ scripts/PlayerManager.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+public partial class PlayerManager : Node3D
+{
+	public override void _Ready()
+	{
+		
+	}
+
+	public override void _Process(double delta)
+	{
+		
+	}
+}
