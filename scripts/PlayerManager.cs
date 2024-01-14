da9cc398b379924e06d6177cb856b54672470120
giggymantis <hypegroupyt@gmail.com>
2024-01-14 15:08:19 -0500

    Added hunger and health variables

--- scripts/PlayerManager.cs
+++ scripts/PlayerManager.cs
@@ -3,6 +3,45 @@ using System;
 
 public partial class PlayerManager : Node3D
 {
+	private float _hunger;
+	public float Hunger 
+	{
+		get 
+		{
+			return _hunger;
+		}
+		set 
+		{
+			if (value <= 100f && value >= 0f) 
+			{
+				_hunger = value;
+			} else 
+			{
+				throw new ArgumentOutOfRangeException("Hunger must be between 100 and 0.");
+			}
+		}
+	};	
+	
+	private float _health;
+	public float Health 
+	{
+		get 
+		{
+			return _health;
+		}
+		set 
+		{
+			if (value <= 100f && value >= 0f) 
+			{
+				_health = value;
+			} else 
+			{
+				throw new ArgumentOutOfRangeException("Health must be between 100 and 0.");
+			}
+		}
+	};	
+	
+	
 	public override void _Ready()
 	{
 		
