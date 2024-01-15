5e62d784329defe1d49e58dc06e5989728b7edb2
giggymantis <hypegroupyt@gmail.com>
2024-01-15 13:17:11 -0500

    Escape Menu Changes

--- scenes/3d_root_node.gd
+++ scenes/3d_root_node.gd
@@ -1,23 +1,16 @@
 extends Node3D
 
-var is_pressed = false
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$EscapeMenu.hide()
 	$EscapeMenu.set_process(false)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
-		if is_pressed == false:
-			$EscapeMenu.set_process(true)
-			$EscapeMenu.show()
-			is_pressed = !is_pressed
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		else:
+		if get_node("EscapeMenu").is_visible():
 			$EscapeMenu.set_process(false)
 			$EscapeMenu.hide()
-			is_pressed = !is_pressed
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-
+		else:
+			$EscapeMenu.set_process(true)
+			$EscapeMenu.show()
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
--- scenes/escape_menu.gd
+++ scenes/escape_menu.gd
@@ -1,16 +1,16 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_quit_button_pressed():
 	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
 	get_tree().quit()
+
+func _on_menu_button_pressed():
+	pass # TODO: Go to main menu
+
+func _on_unpause_button_pressed():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	self.hide()
+	self.set_process(false)
+
+func _on_options_button_pressed():
+	pass # TODO: Go to options menu
