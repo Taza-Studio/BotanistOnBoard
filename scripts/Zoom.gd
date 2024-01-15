8674de5f26e041b0a414c9a9e2e67f6a8beab966
giggymantis <hypegroupyt@gmail.com>
2024-01-15 16:47:21 -0500

    more magnifying glass stuff

--- scripts/Zoom.gd
+++ scripts/Zoom.gd
@@ -3,5 +3,5 @@ extends MeshInstance3D
 @onready var ZoomPos = $CameraPos
 @onready var ZoomCamera = $Magnifier/ZoomCamera
 
-func _process(delta):
+func _process(_delta):
 	ZoomCamera.global_transform = ZoomPos.global_transform
