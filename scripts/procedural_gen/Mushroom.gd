4a8ee07efe7d82ecae9d76ab3ad8af32ce90e5f9
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-17 12:20:29 -0500

    Mesh generation

--- scripts/procedural_gen/Mushroom.gd
+++ scripts/procedural_gen/Mushroom.gd
@@ -9,11 +9,17 @@ export(int, "BLACK", "WHITE", "SILVER", "PINK", "RED", "CRIMSON", "ORANGE", "BRO
 export(int, "BLACK", "WHITE", "SILVER", "PINK", "RED", "CRIMSON", "ORANGE", "BROWN", "YELLOW", "MUSTARD", "CHARTREUSE", "GREEN", "LIME", "CYAN", "LIGHT_BLUE", "DARK_BLUE", "VIOLET", "FUCHSIA") var cap_color_aux
 
 func get_model():
-    var ret = MeshInstance3D.new()
-    var stem_file_location = "res://models/procedural_gen/stem%s.obj"
-    #ret.mesh = load(stem_file_location % )
-
-    return ret
+    var cap = MeshInstance3D.new()
+    var stem = MeshInstance3D.new()
+   
+    var stem_file_location = "res://models/procedural_gen/stem%d.obj"
+    stem.mesh = load(stem_file_location % ((model_specs << 1) % 4))
+    # stem.material_override.albedo_color = 
+    var cap_file_location = "res://models/procedural_gen/cap%d%s.obj"
+    var cap_s = "s" if (model_specs % 2 == 1) else "n"
+    cpa.mesh = load(stem_file_location % [((model_specs << 3) % 4), cap_s])
+    cap.add_child(stem)
+    return cap
 
 # TODO: Spore Print Generation
 
