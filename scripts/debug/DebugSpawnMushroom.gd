e60b9afa6e5afc1ced4738e6546c4ee8739e5bbe
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-21 15:15:49 -0500

    Muted colors

--- scripts/debug/DebugSpawnMushroom.gd
+++ scripts/debug/DebugSpawnMushroom.gd
@@ -4,9 +4,9 @@ func _init():
 	var spotted = ((randi() % 2) == 1)
 	var cap_type = randi() % 3
 	var stem_type = randi() % 3
-	var stem_color = randi() % 19
-	var cap_color = randi() % 19
-	var cap_color_aux = randi() % 19
+	var stem_color = randi() % 18
+	var cap_color = randi() % 18
+	var cap_color_aux = randi() % 18
 	var mushroom_reference = Mushroom.new(spotted,cap_type,stem_type,stem_color,cap_color,cap_color_aux,0.0,1)
 	var mesh:MeshInstance3D = Models.mushroom(mushroom_reference)
 	add_child(mesh)
