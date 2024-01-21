252a7157dba12b6f0e54b6d770fa6d91f00d6f98
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-21 16:35:59 -0500

    language

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -1,34 +1,21 @@
 class_name Inventory
 
-const SLOTS = 10
-
 #region Key Items
 var key_item_journal:bool = true
 #endregion
 
-var items = []
-var old_selected:int = 0
+var item0:ItemSlot
+var item1:ItemSlot
+var item2:ItemSlot
+var item3:ItemSlot
+var item4:ItemSlot
+var item5:ItemSlot
+var item6:ItemSlot
+var item7:ItemSlot
+var item8:ItemSlot
+var item9:ItemSlot
 var selected:int = 0
-var changed:bool = false
 
-func _init():
-	changed = true
-
-func add_slot(item:ItemSlot):
-	changed = true
-	if (items.size() <= SLOTS):
-		items.append(item)
-	else:
-		# TODO: GAME_INVENTORY_YOU_CANNOT_CARRY_ANY_MORE
-		pass
-
-func remove_slot(item):
-	changed = true
-	if (typeof(item) == TYPE_OBJECT):
-		items.erase(item)
-	elif (typeof(item) == TYPE_INT):
-		items.remove_at(item)
-		
 # TODO: func add/remove_item(item:String):
 
 func get_selected_slot() -> ItemSlot:
