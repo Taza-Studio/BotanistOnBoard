6dd82904ebde6fae0edc9b27519553dea496cbc3
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-23 11:12:44 -0500

    Simplified code

--- scripts/3d_root_node.gd
+++ scripts/3d_root_node.gd
@@ -16,27 +16,28 @@ func _process(_delta):
 		get_node("SubViewportContainer/SubViewport/Player").position.x = 5
 		get_node("SubViewportContainer/SubViewport/Player").position.z = 5
 	if Input.is_action_just_pressed("pause"):
+		hide_all()
 		if escapemenu.is_visible():
-			escapemenu.set_process(false)
-			escapemenu.hide()
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			escapemenu.set_process(true)
 			escapemenu.show()
-			inventory_gui.set_process(false)
-			inventory_gui.hide()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	elif Input.is_action_just_pressed("open_inv"):
+		hide_all()
 		if inventory_gui.is_visible():
-			inventory_gui.set_process(false)
-			inventory_gui.hide()
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else:
 			inventory_gui.set_process(true)
 			inventory_gui.show()
-			escapemenu.set_process(false)
-			escapemenu.hide()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
+func hide_all():
+	escapemenu.set_process(false)
+	escapemenu.hide()
+	inventory_gui.set_process(false)
+	inventory_gui.hide()
+			
 func _notification(what):
 	if what == NOTIFICATION_APPLICATION_FOCUS_OUT:
 		escapemenu.set_process(true)
