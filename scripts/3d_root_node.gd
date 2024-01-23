3b5f4a46fe120e93322097c2799bd755a97b2646
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-23 18:33:44 -0500

    More code cleaning on 3d_root_node.gd

--- scripts/3d_root_node.gd
+++ scripts/3d_root_node.gd
@@ -11,10 +11,15 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(_delta):
+	#region Debug
+	#	if player falls of edge, teleport them to the center of the plane
 	if get_node("SubViewportContainer/SubViewport/Player").position.y < -50:
 		get_node("SubViewportContainer/SubViewport/Player").position.y = 10
 		get_node("SubViewportContainer/SubViewport/Player").position.x = 5
 		get_node("SubViewportContainer/SubViewport/Player").position.z = 5
+	#endregion
+	
+	#region Handle UI Inputs
 	if Input.is_action_just_pressed("pause"):
 		hide_all()
 		if not escapemenu.is_visible():
@@ -27,7 +32,8 @@ func _process(_delta):
 			inventory_gui.set_process(true)
 			inventory_gui.show()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			
+	#endregion
+	
 func hide_all():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	escapemenu.set_process(false)
