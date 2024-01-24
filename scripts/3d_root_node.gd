bb46efcc0413f25c17f4d4aeac0619d5f1a3012a
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-23 19:58:02 -0500

    Fixed bug

--- scripts/3d_root_node.gd
+++ scripts/3d_root_node.gd
@@ -18,25 +18,27 @@ func _process(_delta):
 	
 	#region Handle UI Inputs
 	if Input.is_action_just_pressed("pause"):
+		var p = not escapemenu.is_processing()
 		hide_all()
-		if not escapemenu.is_visible():
+		if p:
 			escapemenu.set_process(true)
 			escapemenu.show()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	elif Input.is_action_just_pressed("open_inv"):
+		var p = not inventory_gui.is_processing()
 		hide_all()
-		if not inventory_gui.is_visible():
+		if p:
 			inventory_gui.set_process(true)
 			inventory_gui.show()
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	#endregion
 	
 func hide_all():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	escapemenu.set_process(false)
 	escapemenu.hide()
 	inventory_gui.set_process(false)
 	inventory_gui.hide()
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			
 func _notification(what):
 	if what == NOTIFICATION_APPLICATION_FOCUS_OUT:
