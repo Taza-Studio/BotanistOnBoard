cfc70f652ab153ab6283baa331af8b537287e9e0
Giggy <71663261+GiggyMantis@users.noreply.github.com>
2024-01-26 16:33:07 -0500

    You can now swap items in the inventory

--- scripts/Inventory.gd
+++ scripts/Inventory.gd
@@ -16,6 +16,8 @@ var item8:ItemSlot
 var item9:ItemSlot
 var selected:int = 0
 
+var selected_for_swap:int = -1
+
 # TODO: func add/remove_item(item:String):
 
 # Yes I know this is ugly.
@@ -44,5 +46,37 @@ func get_slot(s:int) -> ItemSlot:
 		_:
 			return get_slot(clamp(s, 0, 9))
 	
+func set_slot(s:int, i:ItemSlot):
+	match s:
+		0:
+			item0 = i
+		1:
+			item1 = i
+		2:
+			item2 = i
+		3:
+			item3 = i
+		4:
+			item4 = i
+		5:
+			item5 = i
+		6:
+			item6 = i
+		7:
+			item7 = i
+		8:
+			item8 = i
+		9:
+			item9 = i
+		_:
+			set_slot(clamp(s,0,9),i)
+	
 func get_selected_slot() -> ItemSlot:
 	return get_slot(selected)
+
+func swap(new_slot:int):
+	var temps = get_slot(selected_for_swap)
+	set_slot(selected_for_swap,get_slot(new_slot))
+	set_slot(new_slot,temps)
+	selected_for_swap = -1
+	
--- scripts/ItemSlotGUI.gd
+++ scripts/ItemSlotGUI.gd
@@ -5,12 +5,21 @@ extends TextureRect
 
 func _process(_delta):
 	if (inventory.get_slot(slotID) == null):
-		texture = preload("res://textures/invisible.png")
+		texture = preload("res://textures/test_image2.png")
 	else:
 		texture = inventory.get_slot(slotID).icon
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and not event.is_echo() and event.is_action("ui_click", true):
-		if get_rect().has_point(event.position):
-			print('test')
+		if get_global_rect().has_point(event.position):
+			print(event.position)
+			swap()
+			
+func swap():
+	if inventory.selected_for_swap == -1:
+		inventory.selected_for_swap = slotID
+	elif inventory.selected_for_swap == slotID:
+		inventory.selected_for_swap = -1
+	else:
+		inventory.swap(slotID)
 		
